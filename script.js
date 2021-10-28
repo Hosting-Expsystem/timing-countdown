b781d615f5dfef3df59c1fa96435384cce276da9
Hosting-Expsystem <91516213+Hosting-Expsystem@users.noreply.github.com>
2021-10-28 16:58:06 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+function getTimeRemaining(endtime) {
+    const total = Date.parse(endtime) - Date.parse(new Date());
+    const seconds = Math.floor((total / 1000) % 60);
+    const minutes = Math.floor((total / 1000 / 60) % 60);
+    const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+    const days = Math.floor(total / (1000 * 60 * 60 * 24));
+    
+    return {
+      total,
+      days,
+      hours,
+      minutes,
+      seconds
+    };
+  }
+  
+  function initializeClock(id, endtime) {
+    const clock = document.getElementById(id);
+    const daysSpan = clock.querySelector('.days');
+    const hoursSpan = clock.querySelector('.hours');
+    const minutesSpan = clock.querySelector('.minutes');
+    const secondsSpan = clock.querySelector('.seconds');
+    
+    function updateClock() {
+      const t = getTimeRemaining(endtime);
+  
+      daysSpan.innerHTML = t.days;
+      hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+      minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+      secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+  
+      if (t.total <= 0) {
+        clearInterval(timeinterval);
+      }
+    }
+  
+    updateClock();
+    const timeinterval = setInterval(updateClock, 1000);
+  }
+
+  var hourx = "";
+  var todayDate = "";
+  var deadline = "";
+
+  setInterval(function(){ 
+    hourx     = new Date().getHours()+1;
+    todayDate = new Date().toISOString().slice(0, 10);
+    deadline  = todayDate+' '+hourx+':00:00';
+    // console.log(deadline);
+    initializeClock('clockdiv', deadline);
+  }, 1000);
